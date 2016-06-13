abbdc431d93206d725b41fb444e7c5e534e51704
82049406@qq.com <82049406@qq.com>
2016-06-13 10:38:13 +0800

    insertion

---
+++ algorithms/sorting/insertion_sort/insertion.go
@@ -0,0 +1,15 @@
+package insertion
+
+import ()
+
+func sort(arr []int) {
+  for i := 1; i < len(arr); i++ {
+    value := arr[i]
+    j := i - 1
+    for j >= 0 && arr[j] > value {
+      arr[j+1] = arr[j]
+      j = j - 1
+    }
+    arr[j+1] = value
+  }
+}
